ffd84d3fdeaee538a9a7e8542aa155169c28edd9
Tiia Björk <tiia.bjork@gmail.com>
2018-02-14 11:19:52 +0200

    Tehtävät 4.1 ja 4.2. check

---
+++ controllers/blogs.js
@@ -0,0 +1,34 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+const formatBlog = (blog) =>  {
+  return {
+    id: blog._id,
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes
+  }
+}
+
+blogsRouter.get('/', (request, response) => {
+  console.log('toimii')
+  //response.send('Hello worldeli')
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs.map(formatBlog))
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = blogsRouter
